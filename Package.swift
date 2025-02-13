364bb615d79bd763c28dcc6a4482505cb5d28884
Danny Pang <danny901113@gmail.com>
2025-02-13 14:28:05 +0800

    move test target to its own package
    this remove the dependency of any html parser.

--- Package.swift
+++ Package.swift
@@ -21,7 +21,6 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
-        .package(url: "https://github.com/scinfu/SwiftSoup.git", .upToNextMajor(from: "2.7.2")),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
@@ -34,11 +33,7 @@ let package = Package(
                 .enableUpcomingFeature("MemberImportVisibility"),
                 .enableUpcomingFeature("InternalImportsByDefault"),
             ]
-        ),
-        .testTarget(
-            name: "HTMLParserBuilderTests",
-            dependencies: ["SwiftSoup", "HTMLParserBuilder"]
-        ),
+        )
     ],
     swiftLanguageVersions: [.version("6"), .v5]
 )
---
+++ Tests/Package.swift
@@ -0,0 +1,28 @@
+// swift-tools-version: 6.0
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "HTMLParserBuilderTests",
+    products: [
+        // Products define the executables and libraries a package produces, making them visible to other packages.
+    ],
+    dependencies: [
+        // Dependencies declare other packages that this package depends on.
+        // .package(url: /* package url */, from: "1.0.0"),
+        .package(path: "../"),
+        .package(url: "https://github.com/scinfu/SwiftSoup.git", .upToNextMajor(from: "2.7.2")),
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package, defining a module or a test suite.
+        // Targets can depend on other targets in this package and products from dependencies.
+        .testTarget(
+            name: "HTMLParserBuilderTests",
+            dependencies: [
+                .product(name: "HTMLParserBuilder", package: "html-parser-builder"),
+                "SwiftSoup"
+            ]
+        ),
+    ]
+)
