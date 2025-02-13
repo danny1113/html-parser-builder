2a4ad766833182a82ae71c5e0ce03929ff17fc5d
Danny Pang <danny901113@gmail.com>
2025-02-14 00:28:55 +0800

    set minimum platform requirements

--- Tests/Package.swift
+++ Tests/Package.swift
@@ -5,6 +5,13 @@ import PackageDescription
 
 let package = Package(
     name: "HTMLParserBuilderTests",
+    platforms: [
+        .macOS(.v10_15),
+        .iOS(.v13),
+        .tvOS(.v13),
+        .watchOS(.v6),
+        .visionOS(.v1),
+    ],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
     ],
